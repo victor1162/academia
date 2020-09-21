6c4527f37e879f09214cb7891ffb281a9aa485c3
Victor1162 <victor_santos1162@hotmail.com>
2020-09-21 16:08:35 -0300

    versao1.3

--- js/alimentacao.js
+++ js/alimentacao.js
@@ -1,3 +1,13 @@
 /***fazendo aplicação rolagem para baixo menu***/
 
 /***Final da aplicação***/
+
+/* modoal suplementação**/
+function abrirModal(){
+	document.getElementById('modal').style.top = '0'
+
+	}
+function fecharModal(){
+	document.getElementById('modal').style.top = '-100%'
+
+	}
